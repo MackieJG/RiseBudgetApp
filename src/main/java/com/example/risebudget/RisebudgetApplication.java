4d0a7180f8d2297e4abd06142896e9990cac901f
MackieJG <joshuamackie10@googlemail.com>
2023-02-27 11:04:12 +0000

    providing first commit for the project

---
+++ src/main/java/com/example/risebudget/RisebudgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.risebudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisebudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisebudgetApplication.class, args);
+	}
+
+}
